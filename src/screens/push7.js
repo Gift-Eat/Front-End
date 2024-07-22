89120b6500aee9168be83182d0a49b93f5e8efe3
ryumoon <ansdlf2810@gmail.com>
2024-07-22 22:53:19 +0900

    Feat: 7일 파일 추가

---
+++ src/screens/push7.js
@@ -0,0 +1,3 @@
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, TouchableOpacity, Alert } from "react-native";
