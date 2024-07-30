36f55d1770ea26929970b3e094f3478154566404
ye6194 <ye6194@naver.com>
2024-07-30 13:13:53 +0900

    Feat: 로그인 페이지 퍼블리싱

---
+++ src/screens/login.js
@@ -0,0 +1,76 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>로그인</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>아이디</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.inputText}>비밀번호</Text>
+        <TextInput style={styles.input} secureTextEntry />
+      </View>
+      <TouchableOpacity style={styles.signup} activeOpacity={0.7}>
+        <Text style={styles.signupText}>회원가입</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn} activeOpacity={0.8}>
+        <Text style={styles.loginBtnText}>로그인</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 100,
+  },
+  inputContainer: {
+    width: "90%",
+  },
+  inputText: {
+    fontSize: 16,
+    marginBottom: 6,
+  },
+  input: {
+    height: 56,
+    borderColor: "#28A745",
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+    fontSize: 16,
+  },
+  signup: {
+    marginTop: 12,
+  },
+  signupText: {
+    color: "#28A745",
+  },
+  loginBtn: {
+    width: "90%",
+    height: 56,
+    marginTop: 160,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#28A745",
+    borderRadius: 15,
+  },
+  loginBtnText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
